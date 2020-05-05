a7496aa9b5ca2693c4369c7e933df5ff41522e7f
MixLab-Tech <spyfari@163.com>
2020-05-05 19:25:01 +0800

    v1.0

--- hacking/regex.js
+++ hacking/regex.js
@@ -1,147 +1,71 @@
-function matchOne(pattern, text) {
-    if (!pattern) return true;
-    if (!text) return false;
-    return pattern === "." || text === pattern;
-}
-
-function search(pattern, text) {
-    if (pattern[0] === "^") {
-        return match(pattern.slice(1), text);
-    } else {
-        return match(".*" + pattern, text);
+class MyRegex {
+    static getInstance(...arg) {
+        if (!MyRegex.instance) MyRegex.instance = new MyRegex(arg);
+        return MyRegex.instance;
     }
-}
-
-function match(pattern, text) {
-    if (!pattern) return true;
-    else if (!text && pattern === "$") return true;
-    else if (pattern[1] === "?") {
-        return matchQuestion(pattern, text);
-    } else if (pattern[1] === "*") {
-        return matchStar(pattern, text);
-    } else if (pattern[0] === "(") {
-        return matchGroup(pattern, text);
-    } else {
-        return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1));
+    constructor() {
+        console.log('运行一次')
     }
-}
-
-function matchQuestion(pattern, text) {
-    return (
-        (matchOne(pattern[0], text[0]) && match(pattern.slice(2), text.slice(1))) ||
-        match(pattern.slice(2), text)
-    );
-}
-
-function matchStar(pattern, text) {
-    return (
-        (matchOne(pattern[0], text[0]) && match(pattern, text.slice(1))) ||
-        match(pattern.slice(2), text)
-    );
-}
-
-function matchGroup(pattern, text) {
-    const groupEnd = pattern.indexOf(")");
-    const groupPattern = pattern.slice(1, groupEnd);
-    if (pattern[groupEnd + 1] === "?") {
-        const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')?'
+    matchOne(pattern, text) {
+        if (!pattern) return true;
+        if (!text) return false;
+        return pattern === "." || text === pattern;
+    }
+    search(pattern, text) {
+        if (pattern[0] === "^") {
+            return this.match(pattern.slice(1), text);
+        } else {
+            return this.match(".*" + pattern, text);
+        }
+    }
+    match(pattern, text) {
+        if (!pattern) return true;
+        else if (!text && pattern === "$") return true;
+        else if (pattern[1] === "?") {
+            return this.matchQuestion(pattern, text);
+        } else if (pattern[1] === "*") {
+            return this.matchStar(pattern, text);
+        } else if (pattern[0] === "(") {
+            return this.matchGroup(pattern, text);
+        } else {
+            return this.matchOne(pattern[0], text[0]) && this.match(pattern.slice(1), text.slice(1));
+        }
+    }
+    matchQuestion(pattern, text) {
         return (
-            (match(groupPattern, text.slice(0, groupPattern.length)) &&
-                match(remainderPattern, text.slice(groupPattern.length))) ||
-            match(remainderPattern, text)
-        );
-    } else if (pattern[groupEnd + 1] === "*") {
-        const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')*'
-        return (
-            (match(groupPattern, text.slice(0, groupPattern.length)) &&
-                match(pattern, text.slice(groupPattern.length))) ||
-            match(remainderPattern, text)
-        );
-    } else {
-        const remainderPattern = pattern.slice(groupEnd + 1); // +1 needed to slice off the ')'
-        return (
-            match(groupPattern, text.slice(0, groupPattern.length)) &&
-            match(remainderPattern, text.slice(groupPattern.length))
+            (this.matchOne(pattern[0], text[0]) && this.match(pattern.slice(2), text.slice(1))) ||
+            this.match(pattern.slice(2), text)
         );
     }
-}
-
-module.exports = {
-    matchOne,
-    match,
-    search
-};
-
-function matchOne(pattern, text) {
-    if (!pattern) return true;
-    if (!text) return false;
-    return pattern === "." || text === pattern;
-}
-
-function search(pattern, text) {
-    if (pattern[0] === "^") {
-        return match(pattern.slice(1), text);
-    } else {
-        return match(".*" + pattern, text);
-    }
-}
-
-function match(pattern, text) {
-    if (!pattern) return true;
-    else if (!text && pattern === "$") return true;
-    else if (pattern[1] === "?") {
-        return matchQuestion(pattern, text);
-    } else if (pattern[1] === "*") {
-        return matchStar(pattern, text);
-    } else if (pattern[0] === "(") {
-        return matchGroup(pattern, text);
-    } else {
-        return matchOne(pattern[0], text[0]) && match(pattern.slice(1), text.slice(1));
-    }
-}
-
-function matchQuestion(pattern, text) {
-    return (
-        (matchOne(pattern[0], text[0]) && match(pattern.slice(2), text.slice(1))) ||
-        match(pattern.slice(2), text)
-    );
-}
-
-function matchStar(pattern, text) {
-    return (
-        (matchOne(pattern[0], text[0]) && match(pattern, text.slice(1))) ||
-        match(pattern.slice(2), text)
-    );
-}
-
-function matchGroup(pattern, text) {
-    const groupEnd = pattern.indexOf(")");
-    const groupPattern = pattern.slice(1, groupEnd);
-    if (pattern[groupEnd + 1] === "?") {
-        const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')?'
+    matchStar(pattern, text) {
         return (
-            (match(groupPattern, text.slice(0, groupPattern.length)) &&
-                match(remainderPattern, text.slice(groupPattern.length))) ||
-            match(remainderPattern, text)
-        );
-    } else if (pattern[groupEnd + 1] === "*") {
-        const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')*'
-        return (
-            (match(groupPattern, text.slice(0, groupPattern.length)) &&
-                match(pattern, text.slice(groupPattern.length))) ||
-            match(remainderPattern, text)
-        );
-    } else {
-        const remainderPattern = pattern.slice(groupEnd + 1); // +1 needed to slice off the ')'
-        return (
-            match(groupPattern, text.slice(0, groupPattern.length)) &&
-            match(remainderPattern, text.slice(groupPattern.length))
+            (this.matchOne(pattern[0], text[0]) && this.match(pattern, text.slice(1))) ||
+            this.match(pattern.slice(2), text)
         );
     }
+    matchGroup(pattern, text) {
+        const groupEnd = pattern.indexOf(")");
+        const groupPattern = pattern.slice(1, groupEnd);
+        if (pattern[groupEnd + 1] === "?") {
+            const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')?'
+            return (
+                (this.match(groupPattern, text.slice(0, groupPattern.length)) &&
+                    this.match(remainderPattern, text.slice(groupPattern.length))) ||
+                this.match(remainderPattern, text)
+            );
+        } else if (pattern[groupEnd + 1] === "*") {
+            const remainderPattern = pattern.slice(groupEnd + 2); // +2 needed to slice off the ')*'
+            return (
+                (this.match(groupPattern, text.slice(0, groupPattern.length)) &&
+                    this.match(pattern, text.slice(groupPattern.length))) ||
+                this.match(remainderPattern, text)
+            );
+        } else {
+            const remainderPattern = pattern.slice(groupEnd + 1); // +1 needed to slice off the ')'
+            return (
+                this.match(groupPattern, text.slice(0, groupPattern.length)) &&
+                this.match(remainderPattern, text.slice(groupPattern.length))
+            );
+        }
+    }
 }
-
-module.exports = {
-    matchOne,
-    match,
-    search
-};
